e00c0238683df6b337cd976ba83ca37b4a98d5b5
ghaoliang <307690509@qq.com>
2019-07-02 20:03:37 +0800

    补充完善获取订单明细和获取订单总金额的方法

--- src/main/java/com/coding/order/service/OrderService.java
+++ src/main/java/com/coding/order/service/OrderService.java
@@ -15,18 +15,35 @@ import java.util.List;
  */
 public class OrderService {
 
-
+    /**
+     * 获取订单明细清单
+     * @param orderItemCommands
+     * @return
+     */
     public List<OrderItemRepresentation> getOrderDetailInfo(List<OrderItemCommand> orderItemCommands ){
         ProductService productService = new ProductService();
         List<OrderItemRepresentation> orderItems = new ArrayList<OrderItemRepresentation>();
         for(OrderItemCommand orderItemCommand : orderItemCommands){
             Product product = productService.getProductInfo(orderItemCommand.getProduct());
+            OrderItemRepresentation orderItem = new OrderItemRepresentation(product.getProductId(),product.getProductName(),product.getPrice(),orderItemCommand.getAmount(),product.getPrice().multiply(orderItemCommand.getAmount()));
+            orderItems.add(orderItem);
         }
         return orderItems;
     }
 
+    /**
+     * 根据订单明细获取订单总金额
+     * @param orderItems
+     * @return
+     */
     public BigDecimal getOrderTotalPrice(List<OrderItemRepresentation> orderItems) {
-        //TODO 计算订单总金额
-        return null;
+        BigDecimal totalPrice = new BigDecimal(0);
+        if(orderItems == null || orderItems.size() == 0){
+            return totalPrice;
+        }
+        for(OrderItemRepresentation orderItemRepresentation : orderItems){
+            totalPrice = totalPrice.add(orderItemRepresentation.getSubTotal());
+        }
+        return totalPrice;
     }
 }
