49f0f4f8b21de10e070085b78c80ffbc30509264
ghaoliang <307690509@qq.com>
2019-07-02 16:42:16 +0800

    定义卡类型信息

---
+++ src/main/java/com/coding/Card/Card.java
@@ -0,0 +1,16 @@
+package com.coding.Card;
+
+/**
+ * 卡信息实体类
+ * @author gaohaoliang
+ */
+public class Card {
+    /**
+     * 卡类型
+     */
+    private String cardType;
+    /**
+     * 积分基准倍数
+     */
+    private String pointTimes;
+}
