b9ee30a96d3fddb92d137300b3e0fe7c2f9de45a
ghaoliang <307690509@qq.com>
2019-07-02 16:57:11 +0800

    卡类型信息set get方法添加

--- src/main/java/com/coding/Card/Card.java
+++ src/main/java/com/coding/Card/Card.java
@@ -13,4 +13,20 @@ public class Card {
      * 积分基准倍数
      */
     private String pointTimes;
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getPointTimes() {
+        return pointTimes;
+    }
+
+    public void setPointTimes(String pointTimes) {
+        this.pointTimes = pointTimes;
+    }
 }
